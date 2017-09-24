5fc2ec6399cdc0183cfa90d199ce7422975c00bf
Oren (electricessence) <oren@electrified.net>
2017-09-23 20:19:59 -0700

    Initial Checkin

---
+++ Lazy.cs
@@ -0,0 +1,20 @@
+﻿/*!
+ * @author electricessence / https://github.com/electricessence/
+ * Licensing: MIT https://github.com/electricessence/Open/blob/dotnet-core/LICENSE.md
+ */
+
+using System.Threading;
+
+namespace System
+{
+    public static class Lazy
+	{
+		public static Lazy<T> Create<T>(Func<T> factory, LazyThreadSafetyMode mode = LazyThreadSafetyMode.ExecutionAndPublication)
+		{
+			if(factory==null)
+				throw new ArgumentNullException("factory");
+
+			return new Lazy<T>(factory, mode);
+		}
+	}
+}
