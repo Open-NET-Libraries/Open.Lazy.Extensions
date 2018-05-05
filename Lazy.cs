f7779e34f3b629dc90c9c1853325d683a96e29a1
Oren (electricessence) <oren@electrified.net>
2018-05-05 15:51:34 -0700

    Reformat and contract improvments.

--- Lazy.cs
+++ Lazy.cs
@@ -3,16 +3,18 @@
  * Licensing: MIT https://github.com/electricessence/Open/blob/dotnet-core/LICENSE.md
  */
 
+using System.Diagnostics.Contracts;
 using System.Threading;
 
 namespace System
 {
-    public static class Lazy
+	public static class Lazy
 	{
 		public static Lazy<T> Create<T>(Func<T> factory, LazyThreadSafetyMode mode = LazyThreadSafetyMode.ExecutionAndPublication)
 		{
-			if(factory==null)
-				throw new ArgumentNullException("factory");
+			if (factory == null)
+				throw new ArgumentNullException(nameof(factory));
+			Contract.EndContractBlock();
 
 			return new Lazy<T>(factory, mode);
 		}
