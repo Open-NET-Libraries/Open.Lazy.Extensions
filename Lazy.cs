68051fce476011b82d2d38900284f7c641063af4
electricessence <5899455+electricessence@users.noreply.github.com>
2022-03-05 09:01:10 -0800

    Updated.
    Now with .Default<T>()

--- Lazy.cs
+++ Lazy.cs
@@ -1,22 +1,24 @@
-﻿/*!
- * @author electricessence / https://github.com/electricessence/
- * Licensing: MIT https://github.com/electricessence/Open/blob/dotnet-core/LICENSE.md
- */
-
-using System.Diagnostics.Contracts;
+﻿using System.Diagnostics.Contracts;
 using System.Threading;
 
-namespace System
-{
-	public static class Lazy
-	{
-		public static Lazy<T> Create<T>(Func<T> factory, LazyThreadSafetyMode mode = LazyThreadSafetyMode.ExecutionAndPublication)
-		{
-			if (factory == null)
-				throw new ArgumentNullException(nameof(factory));
-			Contract.EndContractBlock();
+namespace System;
 
-			return new Lazy<T>(factory, mode);
-		}
+internal static class LazyHelper<T>
+{
+	public static T GetDefault() => default!;
+	public static readonly Lazy<T> Default = Lazy.Create(GetDefault);
+}
+
+public static class Lazy
+{
+	public static Lazy<T> Create<T>(Func<T> factory, LazyThreadSafetyMode mode = LazyThreadSafetyMode.ExecutionAndPublication)
+	{
+		if (factory == null)
+			throw new ArgumentNullException(nameof(factory));
+		Contract.EndContractBlock();
+
+		return new Lazy<T>(factory, mode);
 	}
+
+	public static Lazy<T> Default<T>() => LazyHelper<T>.Default;
 }
