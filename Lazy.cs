e0ac49c7c2f7bf0f1f917912b8fbebea2b6208ae
electricessence <5899455+electricessence@users.noreply.github.com>
2024-11-17 11:37:07 -0800

    Add Lazy class and update project version to 1.4.0
    Modified using directives in Lazy.cs by removing System.Diagnostics.Contracts and adjusting the order. Introduced a new static class Lazy with methods for creating instances of Lazy<T>: FromValue<T>(T value), Create<T>(T value), Create<T>(Func<T> factory, LazyThreadSafetyMode mode), and Default<T>(). Updated project version in Open.Lazy.Extensions.csproj from 1.3.3 to 1.4.0.

--- Lazy.cs
+++ Lazy.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics.Contracts;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Threading;
 
 namespace System;
@@ -14,8 +13,14 @@ internal static class LazyHelper<T>
 #endif
 }
 
+/// <summary>
+/// Factory for creating instances of <see cref="Lazy{T}"/>.
+/// </summary>
 public static class Lazy
 {
+    /// <summary>
+    /// Creates a new instance of <see cref="Lazy{T}"/> with the specified value.
+    /// </summary>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Lazy<T> FromValue<T>(T value)
 #if NETSTANDARD2_1
@@ -24,10 +29,23 @@ public static class Lazy
 		=> new(() => value);
 #endif
 
+    /// <remarks>
+    /// Alias for <see cref="FromValue{T}(T)"/>.
+    /// </remarks>
+    /// <inheritdoc cref="FromValue{T}(T)"/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Lazy<T> Create<T>(T value) => FromValue(value);
+
+    /// <summary>
+    /// Creates a new instance of <see cref="Lazy{T}"/> with the specified factory and thread safety mode.
+    /// </summary>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Lazy<T> Create<T>(Func<T> factory, LazyThreadSafetyMode mode = LazyThreadSafetyMode.ExecutionAndPublication)
 		=> new (factory, mode);
 
+    /// <summary>
+    /// Returns a shared instance where the value is <see langword="default"/> of <typeparamref name="T"/>.
+    /// </summary>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Lazy<T> Default<T>() => LazyHelper<T>.Default;
 }
